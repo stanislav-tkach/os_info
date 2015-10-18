5ab550fa059c0e426e9325b59956b65f45a45b49
Jan Schulte <hello@unexpected-co.de>
2015-10-18 21:49:05 +0200

    add functions to recognize os_type

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
-#[test]
-fn it_works() {
+
+pub mod OsType {
+    use std::process::Command;
+    use std::fs;
+
+    pub enum OSType {
+        Unknown,
+        Redhat,
+        OSX
+    }
+
+    fn file_exists(path: &String) -> bool {
+        let metadata = fs::metadata(path);
+
+        match metadata {
+            Ok(md) => md.is_dir() || md.is_file(),
+            Err(_) => false
+        }
+    }
+
+    fn is_os_x() -> bool {
+        let output = Command::new("sw_vers").output().unwrap();
+        output.status.success()
+    }
+
+    pub fn current_platform() {
+        fn os_type() -> OSType {
+            if file_exists(&"/etc/redhat-release".to_string()) || file_exists(&"/etc/centos-release".to_string()) {
+                OSType::Redhat
+            } else if is_os_x() {
+                OSType::OSX
+            } else {
+                OSType::Unknown
+            }
+        }
+    }
 }
