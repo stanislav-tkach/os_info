874777762b2b5ae9cf0a6836e08ec90d43183501
Jan Schulte <hello@unexpected-co.de>
2015-10-21 17:38:19 +0200

    take lsb_release into account and fix panic bug when not running on os x
    When not running on OS X, the process paniced, because there was the
    attempt to get the sw_vers output which didn't contain anything. Now it
    checks if executing the process was actually successful or not

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@ use std::process::Command;
 use std::fs;
 use std::convert::AsRef;
 use std::path::Path;
+mod lsb_release;
 
 #[derive(Debug)]
 pub enum OSType {
     Unknown,
     Redhat,
-    OSX
+    OSX,
+    Ubuntu
 }
 
 fn file_exists<P: AsRef<Path>>(path: P) -> bool {
@@ -20,16 +22,38 @@ fn file_exists<P: AsRef<Path>>(path: P) -> bool {
 }
 
 fn is_os_x() -> bool {
-    let output = Command::new("sw_vers").output().unwrap();
-    output.status.success()
+    match Command::new("sw_vers").output() {
+        Ok(output) => output.status.success(),
+        Err(_) => false
+    }
+}
+
+fn lsb_release() -> OSType {
+    match lsb_release::from_file("/etc/lsb-release") {
+        Ok(release) => {
+            if release.distro == Some("Ubuntu".to_string()) {
+                OSType::Ubuntu
+            }
+            else {
+                OSType::Unknown
+            }
+        },
+        Err(_) => OSType::Unknown
+    }
+
 }
 
 pub fn current_platform() -> OSType {
-    if file_exists("/etc/redhat-release") || file_exists("/etc/centos-release") {
-        OSType::Redhat
-    } else if is_os_x() {
+    if is_os_x() {
         OSType::OSX
-    } else {
+    }
+    else if file_exists("/etc/lsb-release") {
+        lsb_release()
+    }
+    else if file_exists("/etc/redhat-release") || file_exists("/etc/centos-release") {
+        OSType::Redhat
+    }
+    else {
         OSType::Unknown
     }
 }
