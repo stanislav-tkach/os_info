74f11530012b136bbcb3373fa0bafdb90e8fa9cb
Jan Schulte <hello@unexpected-co.de>
2015-10-24 02:10:39 +0200

    add support for debian operating system
    Apparently Debian doesn't have an /etc/lsb-release file, so now
    lsb_release is called and that output is parsed

--- src/lsb_release.rs
+++ src/lsb_release.rs
@@ -5,10 +5,27 @@ use std::fs::File;
 use std::convert::AsRef;
 use std::path::Path;
 use std::io::Error;
+use std::process::Command;
 
 pub struct LsbRelease {
     pub distro: Option<String>,
     pub version: Option<String>
+}
+
+pub fn retrieve() -> Option<LsbRelease> {
+    let output = match Command::new("lsb_release").arg("-a").output() {
+        Ok(o)  => o,
+        Err(_) =>return None
+    };
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    Some(parse(stdout.to_string()))
+}
+
+pub fn is_available() -> bool {
+    match Command::new("lsb_release").output() {
+        Ok(_) => true,
+        Err(_) => false
+    }
 }
 
 pub fn from_file<P: AsRef<Path>>(path: P) -> Result<LsbRelease, Error> {
@@ -31,8 +48,8 @@ pub fn from_file<P: AsRef<Path>>(path: P) -> Result<LsbRelease, Error> {
 }
 
 pub fn parse(file: String) -> LsbRelease {
-    let distrib_regex = Regex::new(r"DISTRIB_ID=(\w+)").unwrap();
-    let distrib_release_regex = Regex::new(r"DISTRIB_RELEASE=([\w\.]+)").unwrap();
+    let distrib_regex = Regex::new(r"Distributor ID:\s(\w+)").unwrap();
+    let distrib_release_regex = Regex::new(r"Release:\s([\w\.]+)").unwrap();
 
     let distro = match distrib_regex.captures_iter(&file).next() {
         Some(m) => {
--- tests/lsb_release_test.rs
+++ tests/lsb_release_test.rs
@@ -3,21 +3,21 @@ mod lsb_release;
 
 fn file() -> String {
 "
-    DISTRIB_ID=Ubuntu
-    DISTRIB_RELEASE=14.04
-    DISTRIB_CODENAME=trusty
-    DISTRIB_DESCRIPTION=\"Ubuntu 14.04.2 LTS\"
-    ".to_string()
+Distributor ID:	Debian
+Description:	Debian GNU/Linux 7.8 (wheezy)
+Release:	7.8
+Codename:	wheezy
+".to_string()
 }
 
 #[test]
 pub fn test_parses_lsb_distro() {
     let parse_results = lsb_release::parse(file());
-    assert_eq!(parse_results.distro, Some("Ubuntu".to_string()));
+    assert_eq!(parse_results.distro, Some("Debian".to_string()));
 }
 
 #[test]
 pub fn test_parses_lsb_version() {
     let parse_results = lsb_release::parse(file());
-    assert_eq!(parse_results.version, Some("14.04".to_string()));
+    assert_eq!(parse_results.version, Some("7.8".to_string()));
 }
