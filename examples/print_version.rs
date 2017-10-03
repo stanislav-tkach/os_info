4e0dcca89f928dd40aa81decf43235e9533d5071
Stanislav Tkach <stanislav.tkach@xdev.re>
2017-10-03 18:22:39 +0300

    Update example and readme

--- examples/print_version.rs
+++ examples/print_version.rs
@@ -2,5 +2,11 @@ extern crate os_info;
 
 fn main() {
     let info = os_info::get();
+
+    // Print full information:
     println!("OS information: {}", info);
+
+    // Print information separately:
+    println!("Type: {}", info.os_type());
+    println!("Version: {}", info.version());
 }
