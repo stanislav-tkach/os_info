e56d1b00891e8ac30d08b81f97b757637057f67c
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2017-11-02 13:15:03 +0200

    Fixed get version on mac os

--- src/macos/mod.rs
+++ src/macos/mod.rs
@@ -9,12 +9,10 @@ use std::process::Command;
 use {Type, Info, Version};
 
 pub fn current_platform() -> Info {
-    let version = retrieve().map(|x| x.product_version).map_or_else(
-        || Version::unknown,
-        |x| {
-            Version::custom(x, None)
-        },
-    );
+    let version = match retrieve() {
+        Some(v) => Version::custom(v.product_version.unwrap_or("unknown".to_string()), None),
+        None => Version::unknown(),
+    };
     Info {
         os_type: Type::Macos,
         version,
