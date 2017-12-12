bc8c588d00c7d68e0a429f0367c4280cf1cbc590
Matt McCarty <mattmccarty@msn.com>
2017-12-12 01:00:11 +0000

    fixing format errors in build.rs

--- build.rs
+++ build.rs
@@ -6,15 +6,27 @@ fn main() {
 
     // When targeting x86_64-pc-windows-gnu, we need to include the DLL libraries
     // found in the lib/x86_64 directory
-    if var("TARGET").map(|target| target == "x86_64-pc-windows-gnu").unwrap_or(false) {
+    if var("TARGET")
+        .map(|target| target == "x86_64-pc-windows-gnu")
+        .unwrap_or(false)
+    {
         let dir = var("CARGO_MANIFEST_DIR").unwrap();
-        println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lib/x86_64").display());
+        println!(
+            "cargo:rustc-link-search=native={}",
+            Path::new(&dir).join("lib/x86_64").display()
+        )
     }
 
     // When targeting i686-pc-windows-gnu, we need to include the DLL libraries
     // found in the lib/i686 directory
-    if var("TARGET").map(|target| target == "i686-pc-windows-gnu").unwrap_or(false) {
+    if var("TARGET")
+        .map(|target| target == "i686-pc-windows-gnu")
+        .unwrap_or(false)
+    {
         let dir = var("CARGO_MANIFEST_DIR").unwrap();
-        println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lib/i686").display());
+        println!(
+            "cargo:rustc-link-search=native={}",
+            Path::new(&dir).join("lib/i686").display()
+        )
     }
 }
