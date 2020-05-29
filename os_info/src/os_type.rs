f24db72ebe067a34bb2899ac05308a3603937128
Stanislav Tkach <stanislav.tkach@gmail.com>
2020-05-30 00:14:33 +0300

    Sort OS list alphabetically

--- os_info/src/os_type.rs
+++ os_info/src/os_type.rs
@@ -6,48 +6,48 @@ use std::fmt::{self, Display, Formatter};
 #[allow(non_camel_case_types)]
 #[non_exhaustive]
 pub enum Type {
-    /// Unknown operating system.
-    Unknown,
+    /// Alpine Linux (<https://en.wikipedia.org/wiki/Alpine_Linux>).
+    Alpine,
+    /// Amazon Linux AMI (<https://en.wikipedia.org/wiki/Amazon_Machine_Image#Amazon_Linux_AMI>).
+    Amazon,
     /// Android (<https://en.wikipedia.org/wiki/Android_(operating_system)>).
     Android,
-    /// Emscripten (<https://en.wikipedia.org/wiki/Emscripten>).
-    Emscripten,
-    /// Linux based operating system (<https://en.wikipedia.org/wiki/Linux>).
-    Linux,
-    /// Red Hat Linux (<https://en.wikipedia.org/wiki/Red_Hat_Linux>).
-    Redhat,
-    /// Red Hat Enterprise Linux (<https://en.wikipedia.org/wiki/Red_Hat_Enterprise_Linux>).
-    RedHatEnterprise,
-    /// Ubuntu (<https://en.wikipedia.org/wiki/Ubuntu_(operating_system)>).
-    Ubuntu,
-    /// Pop!_OS (<https://en.wikipedia.org/wiki/Pop!_OS>)
-    Pop,
-    /// Debian (<https://en.wikipedia.org/wiki/Debian>).
-    Debian,
     /// Arch Linux (<https://en.wikipedia.org/wiki/Arch_Linux>).
     Arch,
     /// CentOS (<https://en.wikipedia.org/wiki/CentOS>).
     Centos,
+    /// Debian (<https://en.wikipedia.org/wiki/Debian>).
+    Debian,
+    /// Emscripten (<https://en.wikipedia.org/wiki/Emscripten>).
+    Emscripten,
     /// Fedora (<https://en.wikipedia.org/wiki/Fedora_(operating_system)>).
     Fedora,
-    /// Amazon (<https://en.wikipedia.org/wiki/Amazon_Machine_Image#Amazon_Linux_AMI>).
-    Amazon,
-    /// SUSE Linux Enterprise (<https://en.wikipedia.org/wiki/SUSE_Linux_Enterprise>).
-    SUSE,
-    ///openSUSE Linux (<https://en.wikipedia.org/wiki/OpenSUSE>).
-    openSUSE,
-    /// Alpine Linux (<https://en.wikipedia.org/wiki/Alpine_Linux>).
-    Alpine,
-    /// Oracle Linux Server (<https://en.wikipedia.org/wiki/Oracle_Linux>).
-    OracleLinux,
-    /// Solus (<https://en.wikipedia.org/wiki/Solus_(operating_system)>).
-    Solus,
-    /// Manjaro (<https://en.wikipedia.org/wiki/Manjaro>).
-    Manjaro,
+    /// Linux based operating system (<https://en.wikipedia.org/wiki/Linux>).
+    Linux,
     /// Mac OS X/OS X/macOS (<https://en.wikipedia.org/wiki/MacOS>).
     Macos,
+    /// Manjaro (<https://en.wikipedia.org/wiki/Manjaro>).
+    Manjaro,
+    /// openSUSE (<https://en.wikipedia.org/wiki/OpenSUSE>).
+    openSUSE,
+    /// Oracle Linux (<https://en.wikipedia.org/wiki/Oracle_Linux>).
+    OracleLinux,
+    /// Pop!_OS (<https://en.wikipedia.org/wiki/Pop!_OS>)
+    Pop,
+    /// Red Hat Linux (<https://en.wikipedia.org/wiki/Red_Hat_Linux>).
+    Redhat,
+    /// Red Hat Enterprise Linux (<https://en.wikipedia.org/wiki/Red_Hat_Enterprise_Linux>).
+    RedHatEnterprise,
     /// Redox (<https://en.wikipedia.org/wiki/Redox_(operating_system)>).
     Redox,
+    /// Solus (<https://en.wikipedia.org/wiki/Solus_(operating_system)>).
+    Solus,
+    /// SUSE Linux Enterprise Server (<https://en.wikipedia.org/wiki/SUSE_Linux_Enterprise>).
+    SUSE,
+    /// Ubuntu (<https://en.wikipedia.org/wiki/Ubuntu_(operating_system)>).
+    Ubuntu,
+    /// Unknown operating system.
+    Unknown,
     /// Windows (<https://en.wikipedia.org/wiki/Microsoft_Windows>).
     Windows,
 }
