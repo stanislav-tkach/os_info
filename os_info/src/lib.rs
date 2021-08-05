6976625295e197e87cb8e202a66f64cfe448c158
Michael Reim <kraileth@elderlinux.org>
2021-08-05 21:52:40 +0200

    Add netbsd as target_os in one more place

--- os_info/src/lib.rs
+++ os_info/src/lib.rs
@@ -65,7 +65,7 @@ mod info;
 #[cfg(not(windows))]
 mod matcher;
 mod os_type;
-#[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+#[cfg(any(target_os = "dragonfly", target_os = "freebsd", target_os = "netbsd"))]
 mod uname;
 mod version;
 
