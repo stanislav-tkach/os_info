4c0d0354715a95e2f09f17dcbb052abc0c07b14d
toddmartin <kj4ntv@gmail.com>
2021-08-20 11:56:57 -0700

    Increase readability in directive
    rustfmt highlighted a better formatting way. Converted over to that.

--- os_info/src/bitness.rs
+++ os_info/src/bitness.rs
@@ -59,7 +59,12 @@ pub fn get() -> Bitness {
 
 #[cfg(all(
     test,
-    any(target_os = "freebsd", target_os = "linux", target_os = "macos", target_os = "netbsd")
+    any(
+        target_os = "freebsd",
+        target_os = "linux",
+        target_os = "macos",
+        target_os = "netbsd"
+    )
 ))]
 mod tests {
     use super::*;
