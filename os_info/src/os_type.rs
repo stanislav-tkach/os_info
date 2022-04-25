a9260f8cb5224d2789c4caca040295926ce27931
Todd Martin <kj4ntv@gmail.com>
2022-04-25 00:56:24 -0700

    Add Illumos to os types list

--- os_info/src/os_type.rs
+++ os_info/src/os_type.rs
@@ -31,6 +31,8 @@ pub enum Type {
     /// Linux based operating system (<https://en.wikipedia.org/wiki/Linux>).
     HardenedBSD,
     /// HardenedBSD (https://hardenedbsd.org/)
+    Illumos,
+    /// Illumos (https://en.wikipedia.org/wiki/Illumos)
     Linux,
     /// Mac OS X/OS X/macOS (<https://en.wikipedia.org/wiki/MacOS>).
     Macos,
