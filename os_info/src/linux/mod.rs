a00ee73796cbfb4d880213f332af7e437f0404a1
wangmengc <wangmengc@uniontech.com>
2024-04-17 15:36:28 +0800

    add support for Uos Linux

--- os_info/src/linux/mod.rs
+++ os_info/src/linux/mod.rs
@@ -45,6 +45,7 @@ mod tests {
             | Type::Mariner
             | Type::NixOS
             | Type::Nobara
+            | Type::Uos
             | Type::OpenCloudOS
             | Type::openEuler
             | Type::openSUSE
