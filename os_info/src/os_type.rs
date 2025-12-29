8ecab089b28c76a3c3adb88dfb948075f3a32f7f
Jesse L. Zamora <xtremekforever@gmail.com>
2025-12-28 20:55:30 -0500

    Add support for KDE neon in os_info crate

--- os_info/src/os_type.rs
+++ os_info/src/os_type.rs
@@ -63,6 +63,8 @@ pub enum Type {
     Ios,
     /// Kali Linux (https://en.wikipedia.org/wiki/Kali_Linux).
     Kali,
+    /// KDE neon (https://en.wikipedia.org/wiki/KDE_neon).
+    KDEneon,
     /// Linux based operating system (<https://en.wikipedia.org/wiki/Linux>).
     Linux,
     /// Mabox (<https://maboxlinux.org/>).
@@ -155,6 +157,7 @@ impl Display for Type {
             Type::InstantOS => write!(f, "instantOS"),
             Type::Ios => write!(f, "iOS"),
             Type::Kali => write!(f, "Kali Linux"),
+            Type::KDEneon => write!(f, "KDE neon"),
             Type::Macos => write!(f, "Mac OS"),
             Type::MidnightBSD => write!(f, "Midnight BSD"),
             Type::Mint => write!(f, "Linux Mint"),
@@ -217,6 +220,7 @@ mod tests {
             (Type::InstantOS, "instantOS"),
             (Type::Ios, "iOS"),
             (Type::Kali, "Kali Linux"),
+            (Type::KDEneon, "KDE neon"),
             (Type::Linux, "Linux"),
             (Type::Mabox, "Mabox"),
             (Type::Macos, "Mac OS"),
